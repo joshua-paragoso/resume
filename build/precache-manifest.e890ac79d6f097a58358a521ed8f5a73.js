129f679a81fc0b518e10a5672918149473b327d9
joshua-paragoso <paragoso90@gmail.com>
2021-08-26 13:28:49 -0700

    added a build folder to upload to netlify which allows site to be depolyed

---
+++ build/precache-manifest.e890ac79d6f097a58358a521ed8f5a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98ec1cbefca951edfa0e9ef8d226a71",
+    "url": "/joshua-paragoso/resumeindex.html"
+  },
+  {
+    "revision": "05402ccc3b89478f4661",
+    "url": "/joshua-paragoso/resumestatic/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e1ec81b2daffeea7720a",
+    "url": "/joshua-paragoso/resumestatic/js/2.09fbdebb.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/joshua-paragoso/resumestatic/js/2.09fbdebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05402ccc3b89478f4661",
+    "url": "/joshua-paragoso/resumestatic/js/main.02171535.chunk.js"
+  },
+  {
+    "revision": "40654f4e8900dbb97250",
+    "url": "/joshua-paragoso/resumestatic/js/runtime-main.4c02e279.js"
+  }
+]);
